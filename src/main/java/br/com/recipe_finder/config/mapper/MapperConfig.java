d7281a0f399632b8d4539d7e18e34bcba32e56f4
Sczuk <hgreasczuk@gmail.com>
2026-04-20 16:27:40 -0300

    chore(config the mapper): configured the mapper of food

--- src/main/java/br/com/recipe_finder/config/mapper/MapperConfig.java
+++ src/main/java/br/com/recipe_finder/config/mapper/MapperConfig.java
@@ -1,13 +1,18 @@
 package br.com.recipe_finder.config.mapper;
 
 import br.com.recipe_finder.mapper.DrinkMapper;
-import br.com.recipe_finder.mapper.MealMapper;
+import br.com.recipe_finder.mapper.FoodMapper;
 import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MapperConfig {
+
+    @Bean
+    public FoodMapper foodMapper(){
+        return Mappers.getMapper(FoodMapper.class);
+    }
 
     @Bean
     public DrinkMapper drinkMapper(){
